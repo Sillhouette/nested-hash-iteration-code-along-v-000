47d3b8b326adb919fc8376f9660dfa7ded7d215f
Richard Melchior <raustin.melchior@gmail.com>
2018-06-04 06:09:47 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,7 +18,7 @@ def remove_strawberry(contacts)
   contacts.each do |person, details|
     details.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| == "strawberry"}
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
   end
